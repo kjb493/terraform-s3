3436e27101133db4e87d1be1e4818f59c2f369a8
okhai <okhaiitsagwede1234@gmail.com>
2025-06-02 20:04:32 +0200

    creat file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
+
 terraform {
   backend "s3" {
-    bucket = "mybucket"
-    key    = "path/to/my/key"
+    bucket = "terraform-tfstate-bucket-01122024"
+    key    = "june2025/okhai/s3.tfstate"
     region = "eu-west-2"
   }
 }
